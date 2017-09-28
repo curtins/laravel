088bfe98af316490c7043bb2402469f1ff882184
Your Name <you@example.com>
2017-09-28 09:03:42 -0700

    modify schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
-        $schedule->command('rss:load')->everyTenMinutes();
+        $schedule->command('rss:load --force')->everyTenMinutes();
     }
 
     /**
