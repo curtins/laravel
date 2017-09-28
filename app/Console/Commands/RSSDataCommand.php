bdfe9f81b864a3b85933934bdecdcf2e0c27747f
Your Name <you@example.com>
2017-09-28 07:43:06 -0700

    command update

--- app/Console/Commands/RSSDataCommand.php
+++ app/Console/Commands/RSSDataCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Filesystem\Filesystem;
 
 class RSSDataCommand extends Command
 {
@@ -11,14 +13,14 @@ class RSSDataCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'rss:load';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Load RSS Data';
 
     /**
      * Create a new command instance.
@@ -37,6 +39,56 @@ class RSSDataCommand extends Command
      */
     public function handle()
     {
-        //
-    }
+        Log::info('Showing user profile for user: ' .  time() );
+        $file = new Filesystem();
+        //$sourceDir = '/home/forge/superfeedr.scurtin.org/code/data';
+        //$destDir = '/home/forge/horizon.scurtin.org/public/data';
+    
+        $sourceDir =  env('SOURCE_DATA', '/home/forge/superfeedr.scurtin.org/code/data');
+        $destDir   =  env('DEST_DATA','/home/forge/horizon.scurtin.org/public/data');
+    
+    
+        $success = \File::copyDirectory($sourceDir, $destDir);
+    
+    
+        $files = \File::allFiles($destDir);
+    
+        $cnt =0;
+    
+        foreach ($files as $file)
+       { 
+            $data = file_get_contents("$file");
+            $json = json_decode($data,true);
+            $strStatus = $json['status']['code'];
+            $strhttp = $json['status']['http'];
+            $strnextFetch = $json['status']['nextFetch'];
+            $strtitle = $json['title'];
+            if (array_key_exists('permalinkUrl', $json))
+            {
+                //echo "image exists " . $json['permalinkUrl'] . "<br>";
+        
+                }
+        
+                    $strfeed = $json['status']['feed'];
+                    if  (array_key_exists('items',$json))
+                    {
+                        if (($strStatus == '200') && (count($json['items']) > 0) )
+                        {
+                            $cnt++;
+                            echo ($file) .  PHP_EOL ;
+                            echo ($strfeed) . PHP_EOL .PHP_EOL ;
+                            //echo ($strStatus) . '<br>';
+                            //echo ($strtitle) . '<br>';
+        
+                            //echo count($json['items']) . ' - items <br>';
+                            //echo count($json['status']) . PHP_EOL ;
+        
+                            //dd($json);
+                        }         //echo count($json['standardLinks']);
+                    }
+                }
+    
+                echo ($cnt) . PHP_EOL ;
+            }
 }
+ 
