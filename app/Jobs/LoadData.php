5c68ecf15b78d6c943ff66a6e15e2ba768f5e3ec
Your Name <you@example.com>
2017-09-29 13:15:46 -0700

    add job

--- app/Jobs/LoadData.php
+++ app/Jobs/LoadData.php
@@ -7,6 +7,8 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Filesystem\Filesystem;
 
 class LoadData implements ShouldQueue
 {
@@ -29,6 +31,55 @@ class LoadData implements ShouldQueue
      */
     public function handle()
     {
-        //
+        Log::info('Showing user profile for user: ' .  time() );
+        $file = new Filesystem();
+        //$sourceDir = '/home/forge/superfeedr.scurtin.org/code/data';
+        //$destDir = '/home/forge/horizon.scurtin.org/public/data';
+    
+        $sourceDir =  env('SOURCE_DATA', '/home/forge/superfeedr.scurtin.org/code/data');
+        $destDir   =  env('DEST_DATA','/home/forge/horizon.scurtin.org/public/data');
+    
+    
+        $success = \File::copyDirectory($sourceDir, $destDir);
+    
+    
+        $files = \File::allFiles($destDir); 
+    
+        $cnt = 0;
+    
+        foreach ($files as $file)
+       { 
+            $data = file_get_contents("$file");
+            $json = json_decode($data,true);
+            $strStatus = $json['status']['code'];
+            $strhttp = $json['status']['http'];
+            $strnextFetch = $json['status']['nextFetch'];
+            $strtitle = $json['title'];
+            if (array_key_exists('permalinkUrl', $json))
+            {
+                //echo "image exists " . $json['permalinkUrl'] . "<br>";
+        
+                }
+        
+                    $strfeed = $json['status']['feed'];
+                    if  (array_key_exists('items',$json))
+                    {
+                        if (($strStatus == '200') && (count($json['items']) > 0) )
+                        {
+                            $cnt++;
+                            echo ($file) .  PHP_EOL ;
+                            echo ($strfeed) . PHP_EOL .PHP_EOL ;
+                            //echo ($strStatus) . '<br>';
+                            //echo ($strtitle) . '<br>';
+        
+                            //echo count($json['items']) . ' - items <br>';
+                            //echo count($json['status']) . PHP_EOL ;
+        
+                            //dd($json);
+                        }         //echo count($json['standardLinks']);
+                    }
+                }
+    
+                echo ($cnt) . PHP_EOL ;
     }
 }
