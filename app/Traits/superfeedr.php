3a997f3dc5a7102720a989fe182970bb9df877f7
Your Name <you@example.com>
2017-09-30 09:05:46 -0700

    add trait

--- app/Traits/superfeedr.php
+++ app/Traits/superfeedr.php
@@ -1,10 +1,44 @@
 <?php
 
-trait SuperFeedrLoad {
+trait SuperFeedrLoad
+{
  
-    public function share($file)
+    public function StripFile($file)
     {
-    // share this item
-    } 
+        $data = file_get_contents("$file");
+        $json = json_decode($data,true);
+        $strStatus = $json['status']['code'];
+        $strhttp = $json['status']['http'];
+        $strnextFetch = $json['status']['nextFetch'];
+        $strtitle = $json['title'];
+
+        if (array_key_exists('permalinkUrl', $json))
+        {
+            //echo "image exists " . $json['permalinkUrl'] . "<br>";
+
+            
+
+                $strfeed = $json['status']['feed'];
+                if  (array_key_exists('items',$json))
+                {
+                    if (($strStatus == '200') && (count($json['items']) > 0) )
+                    {
+                        $cnt++;
+                        echo ($file) .  PHP_EOL ;
+                        echo ($strfeed) . PHP_EOL .PHP_EOL ;
+                        //echo ($strStatus) . '<br>';
+                        //echo ($strtitle) . '<br>';
+
+                        //echo count($json['items']) . ' - items <br>';
+                        //echo count($json['status']) . PHP_EOL ;
+
+                        //dd($json);
+                    }         //echo count($json['standardLinks']);
+
+                }
+
+        }                    
+    }
+} 
+
  
-}
