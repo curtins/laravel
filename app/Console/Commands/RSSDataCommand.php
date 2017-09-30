b3a5c88d36263a28f6dcf59abd22d905cb8d1104
Your Name <you@example.com>
2017-09-30 09:08:52 -0700

    update superfeedr load

--- app/Console/Commands/RSSDataCommand.php
+++ app/Console/Commands/RSSDataCommand.php
@@ -55,40 +55,12 @@ class RSSDataCommand extends Command
     
         $cnt = 0;
     
-        foreach ($files as $file)
+       foreach ($files as $file)
        { 
-            $data = file_get_contents("$file");
-            $json = json_decode($data,true);
-            $strStatus = $json['status']['code'];
-            $strhttp = $json['status']['http'];
-            $strnextFetch = $json['status']['nextFetch'];
-            $strtitle = $json['title'];
-            if (array_key_exists('permalinkUrl', $json))
-            {
-                //echo "image exists " . $json['permalinkUrl'] . "<br>";
-        
-                }
-        
-                    $strfeed = $json['status']['feed'];
-                    if  (array_key_exists('items',$json))
-                    {
-                        if (($strStatus == '200') && (count($json['items']) > 0) )
-                        {
-                            $cnt++;
-                            echo ($file) .  PHP_EOL ;
-                            echo ($strfeed) . PHP_EOL .PHP_EOL ;
-                            //echo ($strStatus) . '<br>';
-                            //echo ($strtitle) . '<br>';
-        
-                            //echo count($json['items']) . ' - items <br>';
-                            //echo count($json['status']) . PHP_EOL ;
-        
-                            //dd($json);
-                        }         //echo count($json['standardLinks']);
-                    }
-                }
+           
+       }
     
-                echo ($cnt) . PHP_EOL ;
-            }
-}
- 
+       echo ($cnt) . PHP_EOL ;
+             
+    }
+} 
