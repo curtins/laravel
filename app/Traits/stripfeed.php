7db98dd320b05ff6d4590f13c9ea0e7c63fe6d0b
Your Name <you@example.com>
2017-10-02 08:26:23 -0700

    6

---
+++ app/Traits/stripfeed.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Traits;
+use Illuminate\Support\Facades\Log;
+
+trait SuperFeedrLoad
+{
+ 
+    public function StripFile($file)
+    {
+        $data = file_get_contents($file);
+        $json = json_decode($data,true);
+       
+        
+        $cnt = 0;
+
+        if (array_key_exists('permalinkUrl', $json))
+        {
+                $detail = array(
+                    
+                                "code"      => $json['status']['code'],
+                                "http"      => $json['status']['http'],
+                                "nextfetch" => $json['status']['nextFetch'],
+                                "title"     => $json['title'],
+                                "feed"      => $json['status']['feed']
+                    
+                );
+                
+                
+                if  (array_key_exists('items',$json))
+                {
+                    if (($json['status']['code'] == '200') && (count($json['items']) > 0) )
+                    {
+                        $cnt++;                        
+                        //Log::info('Decoding.. ' . $file . ',' . $strfeed );
+                         
+                    }         
+                    
+                }
+
+        }    
+                        
+    }
+
+    
+     
+
+
+
+
+
+
+
+
+
+
+
+
+} 
+
+ 
