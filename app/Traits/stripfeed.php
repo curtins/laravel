e63772149d18ce9897aff9f159f2dba33eb05b1e
Your Name <you@example.com>
2017-10-05 08:28:38 -0700

    6

--- app/Traits/stripfeed.php
+++ app/Traits/stripfeed.php
@@ -57,7 +57,7 @@ trait ProcessFile
                 
                         ));
 
-                        $title.save();
+                        //$title.save();
                          
                     }         
                     
