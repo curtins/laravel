ed1691022583087252e147a508949985682f5a5b
Your Name <you@example.com>
2017-10-06 08:21:07 -0700

    6

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 });
 
 Route::get('/headers', function () {
-    return view('headers', compact('newsheader'));
+    return view('headers')->with('newsheader', $newsheaders);
 });
 
 Auth::routes();
