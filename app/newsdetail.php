6227c6cfafde9f2e5e6002628cbdd4d73d2a0a89
Your Name <you@example.com>
2017-10-09 08:07:16 -0700

    6

--- app/newsdetail.php
+++ app/newsdetail.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class newsdetail extends Model
 {
     protected $guarded = [];
+
+
+    public function  newsdetail()
+    {
+        $this-belongsTo('App\newsheader','headerid');
+    }
+
 }
--- app/newsheader.php
+++ app/newsheader.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class newsheader extends Model
 {
     protected $guarded = [];
+
+
+    public function  newsdetail()
+    {
+        $this->hasMany('App\newsdetail','headerid');
+    }
+
+
+
+
+
 }
