0b842116b82748bd6f8f2fd8e1335d7b4fc06b42
Your Name <you@example.com>
2017-10-13 10:42:30 -0700

    9

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -14,7 +14,7 @@
 
                             <tr><td><td>{{$details->feed}} </td>
                             
-                            <td><a href="{{$details->title}}">{{$details->itemid}}</a></td>
+                            <td><a href="{{$details->itemid}}">{{$details-title}}</a></td>
                             
                             </tr> 
                     
